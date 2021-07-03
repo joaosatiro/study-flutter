925f1f3a5bc473fd3cbe2aaa10990fd053eb0468
João Sátiro <joao.v.satiro@gmail.com>
2021-07-03 13:22:05 -0300

    init configuration

---
+++ meals/lib/screens/categories_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:meals/components/category_item.dart';
+import 'package:meals/data/dummy.data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar?'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 250,
+          childAspectRatio: 4 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) {
+          return CategoryItem(
+            category: cat,
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
