71cecb671577abca1d0f6b4da06ddd5b823718d8
João Sátiro <joao.v.satiro@gmail.com>
2021-07-03 14:20:46 -0300

    add images in meal

--- meals/lib/screens/categories_meals_screen.dart
+++ meals/lib/screens/categories_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:meals/components/meal.item.dart';
+import 'package:meals/data/dummy.data.dart';
 import '../models/category.dart' as categoriesMealsScreen;
 
 class CategoriesMealsScreen extends StatelessWidget {
@@ -12,12 +14,19 @@ class CategoriesMealsScreen extends StatelessWidget {
     final category = ModalRoute.of(context)!.settings.arguments
         as categoriesMealsScreen.Category;
 
+    final categoryMeal = DUMMY_MEALS.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(category.title),
       ),
-      body: Center(
-        child: Text('Receita por Categoria ${category.id}'),
+      body: ListView.builder(
+        itemCount: categoryMeal.length,
+        itemBuilder: (ctx, index) {
+          return MealItem(meal: categoryMeal[index]);
+        },
       ),
     );
   }
