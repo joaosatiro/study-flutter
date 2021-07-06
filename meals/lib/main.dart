d06562e75353a7da69bafda09f9d948562e1e322
João Sátiro <joao.v.satiro@gmail.com>
2021-07-05 22:31:18 -0300

    create and finish meal-detail

--- meals/lib/main.dart
+++ meals/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meals/screens/categories_meals_screen.dart';
 import 'package:meals/screens/categories_screen.dart';
+import 'package:meals/screens/meal_detail_screen.dart';
 import 'package:meals/utils/app_route.dart';
 
 void main() => runApp(MyApp());
@@ -25,7 +26,26 @@ class MyApp extends StatelessWidget {
       routes: {
         AppRoutes.HOME: (ctx) => CategoriesScreen(),
         AppRoutes.CATEGORIES_MEALS: (ctx) => CategoriesMealsScreen(),
+        AppRoutes.MEAL_DETAIL: (ctx) => MealDetailScreen(),
       },
+      // Tratamento de rotas:
+
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == 'alguma-coisa') {
+      //     return null;
+      //   } else if (settings.name == 'outra-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(builder: (_) {
+      //       return CategoriesScreen();
+      //     });
+      //   }
+      // },
+      // onUnknownRoute: (settings) {
+      //   return MaterialPageRoute(builder: (_) {
+      //     return CategoriesScreen();
+      //   });
+      // },
     );
   }
 }
